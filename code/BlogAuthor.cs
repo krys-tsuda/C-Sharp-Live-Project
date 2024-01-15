a892a0b07383841ef8514e1b91cf25aff2fd0e6a
kxxy:kodez <135093578+krys-tsuda@users.noreply.github.com>
2024-01-15 14:02:02 -0800

    C-Sharp Live Project Update
    Updated code snippets

---
+++ code/BlogAuthor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheatreCMS3.Areas.Blog.Models
+{
+    public class BlogAuthor
+    {
+        public int BlogAuthorId { get; set; }
+
+        //Required field
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+        // Required field
+        public DateTime Joined { get; set; }
+        
+        // Optional field
+        public DateTime? Left { get; set; }
+    }
+}
